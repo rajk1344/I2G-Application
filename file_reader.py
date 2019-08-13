c565bb4eccf7228034ccc1479ed34767cb70cfb1
kiran <kiran@kiranraj.us>
2019-08-13 11:43:33 -0700

    added functionality for the contract generator, however there needs to be major fixes in the read_matched_students method

--- file_reader.py
+++ file_reader.py
@@ -1,7 +1,8 @@
 import csv
 from student import *
 from project import *
-import missing_student
+from write import *
+from missing_student import *
 
 def read_students(file):
     students = []
@@ -59,3 +60,30 @@ def read_catcources(file):
             catcourse.append(Student(0,row[0], row[1], row[2],0,temp))
 
     return catcourse
+
+#TODO This needs to be fixed
+def read_matched_students(file, output):
+    with open(file, 'r', encoding='utf-8-sig') as csvfile:
+        # create csvread obj
+
+        students2 = []
+        projects2 = []
+
+        csvread = csv.reader(csvfile)
+        temp = []
+        # populate projects
+        for row in csvread:
+            students2.append(Student(row[0], row[1], row[2],row[3],row[4],0))
+            projects2.append(Project(row[5],row[6],row[7],row[8],row[9],row[10]))
+        t = 1
+        p = 1
+        projects2 = list(dict.fromkeys(projects2))
+        project_list = []
+        while t <= 10:
+            student_list = []
+            for student in students2:
+                if str(t) == student.contract:
+                    student_list.append(student)
+            write_project_pdf_contract(student_list, projects2[p],t, output)
+            t += 1
+            p += len(student_list)
--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
 from file_reader import *
-from missing_student import *
 from matcher import *
+from missing_student import *
 
 
 class I2Gapp(tk.Tk):
@@ -19,7 +19,7 @@ class I2Gapp(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, StudentMatcher, CatCoursesMatcher):
+        for F in (StartPage, StudentMatcher, CatCoursesMatcher, ContractCreator):
             frame = F(container, self)
 
             self.frames[F] = frame
@@ -53,7 +53,8 @@ class StartPage(tk.Frame):
         self.button2.grid(column=1, row=3, padx=20, pady=5)
 
         self.button3 = tk.Button(
-            self, text="Contract Creator")
+            self, text="Contract Creator",
+            command=lambda: controller.show_frame(ContractCreator))
         self.button3.grid(column=1, row=4, padx=20, pady=5)
 
         self.grid_rowconfigure(0, weight=1)
@@ -67,7 +68,7 @@ class StudentMatcher(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Student Matcher")
-        label.grid(column=1, row=1, padx=20, pady=20)
+        label.grid(column=2, row=1, padx=20, pady=20)
 
         self.minsize = (1000, 1000)
         self.student_file_button()
@@ -75,57 +76,64 @@ class StudentMatcher(tk.Frame):
         self.output_directory_button()
         self.run_button()
 
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(9, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+
         self.button = tk.Button(
             self, text="<- BACK",
             command=lambda: controller.show_frame(StartPage))
-        self.button.grid(column=0, row=8, padx=20, pady=10)
+        self.button.grid(column=1, row=8, padx=20, pady=10)
 
     def student_file_button(self):
         self.button = tk.Button(
-            self, text="Select Student Roster", 
+            self, text="Select Student Roster",
             command=self.student_file_dialog)
-        self.button.grid(column=1, row=2, padx=20, pady=5)
+        self.button.grid(column=2, row=2, padx=20, pady=5)
 
     def student_file_dialog(self):
         self.student_filename = filedialog.askopenfile(
-            initialdir="/", 
-            title="Select Student Roster", 
+            initialdir="/",
+            title="Select Student Roster",
             filetype=(("Comma-sperated Values", "*.csv"), ("All Files", "*.*")))
         self.label = tk.Label(self, text=self.student_filename.name)
-        self.label.grid(column=1, row=3, padx=20, pady=0)
+        self.label.grid(column=2, row=3, padx=20, pady=0)
 
     def project_file_button(self):
         self.button = tk.Button(
-            self, text="Select Project Roster", 
+            self, text="Select Project Roster",
             command=self.project_file_dialog)
-        self.button.grid(column=1, row=4, padx=20, pady=5)
+        self.button.grid(column=2, row=4, padx=20, pady=5)
 
     def project_file_dialog(self):
         self.project_filename = filedialog.askopenfile(
-            initialdir="/", 
-            title="Select Project Roster", 
+            initialdir="/",
+            title="Select Project Roster",
             filetype=(("Comma-sperated Values", "*.csv"), ("All Files", "*.*")))
         self.label = tk.Label(self, text=self.project_filename.name)
-        self.label.grid(column=1, row=5, padx=20, pady=0)
+        self.label.grid(column=2, row=5, padx=20, pady=0)
 
     def output_directory_button(self):
         self.button = tk.Button(
             self, text="Select Output Location", command=lambda: self.output_directory_dialog())
-        self.button.grid(column=1, row=6, padx=20, pady=5)
+        self.button.grid(column=2, row=6, padx=20, pady=5)
 
     def output_directory_dialog(self):
         self.output_location = filedialog.askdirectory()
         self.label = tk.Label(self, text=self.output_location)
-        self.label.grid(column=1, row=7, padx=20, pady=0)
+        self.label.grid(column=2, row=7, padx=20, pady=0)
 
     def run_button(self):
-        self.button = tk.Button(self, text="RUN ->", command = lambda:self.runner())
-        self.button.grid(column=2, row=8, padx=20, pady=10)
+        self.button = tk.Button(self, text="RUN ->",
+                                command=lambda: self.runner())
+        self.button.grid(column=3, row=8, padx=20, pady=10)
 
     def runner(self):
         self.students = read_students(self.student_filename.name)
         self.projects = read_projects(self.project_filename.name)
         match(self.students, self.projects, self.output_location)
+        messagebox.showinfo("Title", "Done")
 
 
 class CatCoursesMatcher(tk.Frame):
@@ -133,7 +141,7 @@ class CatCoursesMatcher(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="CatCourses Matcher")
-        label.grid(column=1, row=1, padx=20, pady=20)
+        label.grid(column=2, row=1, padx=20, pady=20)
 
         self.minsize = (1000, 1000)
         self.qualtrics_file_button()
@@ -141,53 +149,115 @@ class CatCoursesMatcher(tk.Frame):
         self.output_directory_button()
         self.run_button()
 
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(9, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+
         self.button = tk.Button(
             self, text="<- BACK",
             command=lambda: controller.show_frame(StartPage))
-        self.button.grid(column=0, row=8, padx=20, pady=10)
+        self.button.grid(column=1, row=8, padx=20, pady=10)
 
     def qualtrics_file_button(self):
         self.button = tk.Button(
             self, text="Select Qualtrics Roster", command=self.qualtrics_file_dialog)
-        self.button.grid(column=1, row=2, padx=20, pady=5)
+        self.button.grid(column=2, row=2, padx=20, pady=5)
 
     def qualtrics_file_dialog(self):
         self.qualtrics_filename = filedialog.askopenfile(
             initialdir="/", title="Select Qualtrics Roster", filetype=(("Comma-sperated Values", "*.csv"), ("All Files", "*.*")))
         self.label = tk.Label(self, text=self.qualtrics_filename.name)
-        self.label.grid(column=1, row=3, padx=20, pady=0)
+        self.label.grid(column=2, row=3, padx=20, pady=0)
 
     def catcources_file_button(self):
         self.button = tk.Button(
             self, text="Select Catcources Roster", command=self.catcources_file_dialog)
-        self.button.grid(column=1, row=4, padx=20, pady=5)
+        self.button.grid(column=2, row=4, padx=20, pady=5)
 
     def catcources_file_dialog(self):
         self.catcources_filename = filedialog.askopenfile(
             initialdir="/", title="Select Catcources Roster", filetype=(("Comma-sperated Values", "*.csv"), ("All Files", "*.*")))
         self.label = tk.Label(self, text=self.catcources_filename.name)
-        self.label.grid(column=1, row=5, padx=20, pady=0)
+        self.label.grid(column=2, row=5, padx=20, pady=0)
 
     def output_directory_button(self):
         self.button = tk.Button(
             self, text="Select Output Location", command=lambda: self.output_directory_dialog())
-        self.button.grid(column=1, row=6, padx=20, pady=5)
+        self.button.grid(column=2, row=6, padx=20, pady=5)
 
     def output_directory_dialog(self):
         self.output_location = filedialog.askdirectory()
         self.label = tk.Label(self, text=self.output_location)
-        self.label.grid(column=1, row=7, padx=20, pady=0)
+        self.label.grid(column=2, row=7, padx=20, pady=0)
 
     def run_button(self):
-        self.button = tk.Button(self, text="RUN ->", command = lambda: self.runner())
-        self.button.grid(column=2, row=8, padx=20, pady=10)
+        self.button = tk.Button(self, text="RUN ->",
+                                command=lambda: self.runner())
+        self.button.grid(column=3, row=8, padx=20, pady=10)
 
     def runner(self):
         self.qualtrics = read_students(self.qualtrics_filename.name)
         self.catcources = read_catcources(self.catcources_filename.name)
-        
-        export_missing_students(list_missing(self.catcources,self.qualtrics),self.output_location)
-        #TODO ask dev on the missing students
+        export_missing_students(list_missing(
+            self.catcources, self.qualtrics), self.output_location)
+        messagebox.showinfo("Title", "Done")
+
+
+class ContractCreator(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Student Matcher")
+        label.grid(column=2, row=1, padx=20, pady=20)
+
+        self.minsize = (1000, 1000)
+        self.master_file_button()
+        self.output_directory_button()
+        self.run_button()
+
+        self.button = tk.Button(
+            self, text="<- BACK",
+            command=lambda: controller.show_frame(StartPage))
+        self.button.grid(column=1, row=8, padx=20, pady=10)
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(9, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+
+    def master_file_button(self):
+        self.button = tk.Button(
+            self, text="Select Master Roster",
+            command=self.master_file_dialog)
+        self.button.grid(column=2, row=2, padx=20, pady=5)
+
+    def master_file_dialog(self):
+        self.master_filename = filedialog.askopenfile(
+            initialdir="/",
+            title="Select Master Roster",
+            filetype=(("Comma-sperated Values", "*.csv"), ("All Files", "*.*")))
+        self.label = tk.Label(self, text=self.master_filename.name)
+        self.label.grid(column=2, row=3, padx=20, pady=0)
+
+    def output_directory_button(self):
+        self.button = tk.Button(
+            self, text="Select Output Location", command=lambda: self.output_directory_dialog())
+        self.button.grid(column=2, row=6, padx=20, pady=5)
+
+    def output_directory_dialog(self):
+        self.output_location = filedialog.askdirectory()
+        self.label = tk.Label(self, text=self.output_location)
+        self.label.grid(column=2, row=7, padx=20, pady=0)
+
+    def run_button(self):
+        self.button = tk.Button(self, text="RUN ->",
+                                command=lambda: self.runner())
+        self.button.grid(column=3, row=8, padx=20, pady=10)
+
+    def runner(self):
+        read_matched_students(self.master_filename.name, self.output_location)
+        messagebox.showinfo("Title", "Done")
+        pass
 
 
 app = I2Gapp()
