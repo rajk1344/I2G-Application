ae4efd7fb55687eb6dc510db57829d636293889b
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-11-29 11:54:07 -0800

    Added the functionality for reading projects from projects log

---
+++ updated_run.py
@@ -0,0 +1,12 @@
+import csv
+from matcher import *
+from project import *
+from student import *
+from missing_student import *
+from write import *
+from file_reader import *
+
+source = 'sample projects log'
+semester = '2019-08-Fall-CAP'
+destination = 'projects-API'
+get_projects_semester(source,destination,semester)
