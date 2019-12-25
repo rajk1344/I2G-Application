8f73a1101d26396734894b45a41fe316cb8a94c8
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-12-24 17:35:48 -0800

    updating change

--- updated_run.py
+++ updated_run.py
@@ -6,7 +6,12 @@ from missing_student import *
 from write import *
 from file_reader import *
 
+
+"""
+Testing the writing Projects log 
 source = 'sample projects log'
 semester = '2019-08-Fall-CAP'
 destination = 'projects-API'
 get_projects_semester(source,destination,semester)
+"""
+
