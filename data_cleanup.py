7a73128fb60da05e37f2939b5aa7c912ffcb5aa2
Devanshu Kumar <devanshu.kumar07@gmail.com>
2020-01-05 13:57:26 -0800

    updating files

---
+++ data_cleanup.py
@@ -0,0 +1,41 @@
+import qualtrics_data_processing
+import csv
+
+"""
+This functionality does the following:
+    1) Remove all the entries where students have disagreed 
+    2) Keep the updated responses based on timestamp
+"""
+def remove_disagreed_students(clean_data):
+    new_clean_data = clean_data
+    i = 0 
+    while i < len(new_clean_data):
+        if str(new_clean_data[i].agreement) != "I agree":
+            del new_clean_data[i]
+        else:
+            i = i + 1
+    return new_clean_data
+
+def remove_duplicate_entries(clean_data):
+    new_clean_data = clean_data
+    i = 0
+    while i < len(new_clean_data):
+        duplicate = False
+        s = new_clean_data[i]
+        k = i + 1
+        while k < len(new_clean_data):
+            sub_student = new_clean_data[k]
+            if sub_student.email == s.email:
+                del new_clean_data[i]
+                duplicate = True
+                break
+            k = k + 1
+        if duplicate == False:
+            i = i + 1
+    return new_clean_data
+
+def clean_data_students(qualtrics, clean_data):
+
+    new_clean_data = remove_disagreed_students(clean_data)
+    new_clean_data = remove_duplicate_entries(new_clean_data)
+    return new_clean_data
---
+++ testing.py
@@ -0,0 +1,32 @@
+import csv
+from matcher import *
+from project import *
+from student import *
+from qualtrics_data_processing import *
+from write import *
+from file_reader import *
+from new_matcher import *
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.algorithms.flow import *
+from networkx.algorithms.bipartite import hopcroft_karp_matching
+from Team import *
+from data_cleanup import *
+import unittest
+
+class testApp(unittest.TestCase):
+   
+   def test_qualtrics_data_processing_functionality(self):
+      students = read_students_new('data/input/Test_data/student_data1.csv')
+      projects = read_projects('data/input/Test_data/projects.csv')
+      catcourse = read_catcources('data/input/Test_data/roster.csv')
+      missing_students = ['dwilley','jroot']
+      output = list_missing(catcourse,students)
+      res = []
+      for o in output:
+         res.append(o.email)
+      self.assertListEqual(res,missing_students)
+
+
+if __name__ == '__main__':
+    unittest.main()
